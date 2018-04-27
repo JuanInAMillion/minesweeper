b277ac3694ac2addcf67528159248662006fb512
Juan Alcantara <juan231f@yahoo.com>
2018-04-27 10:19:32 -0400

    changed let variable to const where it was appropriate

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,5 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  let board = [];
+  const board = [];
   for (let rowsIndex = 0; rowsIndex < numberOfRows; rowsIndex++) {
     let row = [];
     for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++ ) {
@@ -11,9 +11,9 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 };
 
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  let board = [];
+  const board = [];
   for (let rowsIndex = 0; rowsIndex < numberOfRows; rowsIndex++) {
-    let row = [];
+    const row = [];
     for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++ ) {
       row.push(null);
     }
@@ -21,8 +21,8 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   }
   let numberOfBombsPlaced = 0;
   while (numberOfBombsPlaced < numberOfBombs) {
-    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
     board[randomRowIndex][randomColumnIndex] = 'B';
     numberOfBombsPlaced++;
     //bombs will be placed ontop of other bombs, will fix later.
@@ -34,8 +34,8 @@ const printBoard = (board) => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-let playerBoard = generatePlayerBoard(3, 4);
-let bombBoard = generateBombBoard(3, 4, 5);
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
 
 console.log('Player Board: ');
 printBoard(playerBoard);
